b596802e2f7171726940afc4df2331a17befa33f
Rob Landry <rlandry@pemaquid.com>
2013-07-05 21:15:58 -0400

    Create getTweets.js

---
+++ getTweets.js
@@ -0,0 +1,16 @@
+$(function() {
+  // Twitter feed
+	$.ajax({
+		url: 'YOUR_URL',
+		type: 'POST',
+		dataType: 'json',
+		success: function(data, textStatus, xhr) {
+			var html = '';
+			for (var i = 0; i < data.length; i++) {
+        // Adjust the markup to suit your needs
+				html = html +'<li class="latest-tweet"><p>' + data[i] + '</p></li>';
+			}
+		$('#mytweets').append(html);
+		}
+	});
+});
