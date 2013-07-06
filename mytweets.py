f6db6016999e54edeedcda9c85548aa03134339c
Rob Landry <rlandry@pemaquid.com>
2013-07-05 20:52:22 -0400

    Create mytweets.py

---
+++ mytweets.py
@@ -0,0 +1,82 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
+from django.http import HttpResponse
+import json
+
+# Settings -- you could move these to your settings.py and import them
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+CONSUMER_KEY = "6z9XxZTiNdf9n4XY3cXkkA"
+CONSUMER_SECRET = "6DMT8CsEuwmLQKcA0ffUjETCar4KQSR21paMqhkh0Q"
+
+OAUTH_TOKEN = "8321102-edRm5ZHRqCISDa11hya5dCYXsfvCHLGdHqZXcVQjU"
+OAUTH_TOKEN_SECRET = "WRoQujEAlWUghozKMCtfw0BhsbSWUnx0uAj35QuqZM"
+
+SCREEN_NAME = 'gopleinair'
+TWEET_COUNT = '1'
+
+def setup_oauth():
+    """Authorize your app via identifier."""
+    # Request token
+    oauth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET)
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+
+    resource_owner_key = credentials.get('oauth_token')[0]
+    resource_owner_secret = credentials.get('oauth_token_secret')[0]
+    
+    # Authorize
+    authorize_url = AUTHORIZE_URL + resource_owner_key
+    print 'Please go here and authorize: ' + authorize_url
+    
+    verifier = raw_input('Please input the verifier: ')
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=resource_owner_key,
+                   resource_owner_secret=resource_owner_secret,
+                   verifier=verifier)
+
+    # Finally, Obtain the Access Token
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    token = credentials.get('oauth_token')[0]
+    secret = credentials.get('oauth_token_secret')[0]
+
+    return token, secret
+
+
+def get_oauth():
+    oauth = OAuth1(CONSUMER_KEY,
+                client_secret=CONSUMER_SECRET,
+                resource_owner_key=OAUTH_TOKEN,
+                resource_owner_secret=OAUTH_TOKEN_SECRET)
+    return oauth
+
+if __name__ == "__main__":
+    if not OAUTH_TOKEN:
+        token, secret = setup_oauth()
+        print "OAUTH_TOKEN: " + token
+        print "OAUTH_TOKEN_SECRET: " + secret
+        print
+    else:
+        oauth = get_oauth()
+        r = requests.get(url="https://api.twitter.com/1.1/statuses/mentions_timeline.json", auth=oauth)
+        print r.json()
+
+
+# ------------------------------------------------------------------------------------------
+
+def get_recent_tweets(request):
+    lResume = []
+    oauth = get_oauth()
+    url_string = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + SCREEN_NAME + "&count=" + TWEET_COUNT
+    r = requests.get(url=url_string, auth=oauth)
+    lTweets = r.json()
+    for dTweet in lTweets:
+        lResume.append(dTweet['text'])
+    return HttpResponse(json.dumps(lResume),mimetype="application/json")
